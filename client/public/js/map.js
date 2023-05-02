946bbac05e308d6f200dda96a9f6fa8a759ef08b
averrani <aris.issad@gmail.com>
2023-05-02 15:27:30 +0200

    ajout carte

---
+++ client/public/js/map.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([43.296482, 5.36978], 10);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+OpenStreetMap_Mapnik.addTo(map);
