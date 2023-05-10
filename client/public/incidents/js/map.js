89e1c18b8a9a8267db8b68829a5acc20678c304d
averrani <aris.issad@gmail.com>
2023-05-10 10:29:44 +0200

    cleaning

--- client/public/incidents/js/map.js
+++ client/public/incidents/js/map.js
@@ -1,13 +1,5 @@
 var map = L.map('map').setView([43.296482, 5.36978], 10);
 
-1 : 43.352902, 5.285431
-2 : 43.358366219149865, 5.556944074087913
-3 : 43.213288, 5.284260
-4 : 43.216087, 5.571010
-
-
-
-
 var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
