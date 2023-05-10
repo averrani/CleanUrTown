feae5680e5d3e49b5bcb09a17a46ab3aa35c12fd
averrani <aris.issad@gmail.com>
2023-05-10 10:05:46 +0200

    update

--- client/public/customers/js/script.js
+++ client/public/customers/js/script.js
@@ -1,21 +1,21 @@
 //fonction qui recupperer les données du formulaire et les envoie au serveur
 
 document.addEventListener('DOMContentLoaded', () => {
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
-  let ber = "Email : " + getCookie('login');
-  document.getElementById("email").innerHTML = ber;
+  return "";
+}
+let ber = "Email : " + getCookie('login');
+document.getElementById("email").innerHTML = ber;
 });
